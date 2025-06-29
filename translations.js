a09a6ff411d8dfa1fe097123c2b324ab58b6b7aa
hochukushatya <yeldosova@gmail.com>
2025-06-29 17:31:05 +0500

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,64 @@
+const translations = {
+  content: {
+    logo: {
+      kz: "Қазақстан",
+      ru: "Казахстанская ассоциация спортивных федераций гандбола",
+      en: "Kazakhstan Handball Sports Federations Association",
+    },
+    whatsapp: {
+      kz: "Whatsapp-қа жазу",
+      ru: "Написать в Whatsapp",
+      en: "Text in Whatsapp",
+    },
+    mail: {
+    	kz: "Поштаға жазу",
+    	ru: "Написать на почту",
+    	en: "Text mail",
+    }
+  },
+
+  user_1: {
+    full_name: {
+      kz: "Исаева Жибек Сериковна",
+      ru: "Исаева Жибек Сериковна",
+      en: "Isaeva Zhibek Serikovna",
+    },
+    description: {
+      kz: "Вице-президент",
+      ru: "Вице-президент казахстанской ассоциации спортивных федераций гандбола",
+      en: "Vice President of Kazakhstan Handball Federation",
+    },
+    whatsapp_number: "+77075214366",
+    mail_login: "mmukataev.02@gmail.com"
+  },
+
+  user_2: {
+    full_name: {
+      kz: "Жанар Сарсенова",
+      ru: "Жанар Сарсенова",
+      en: "Zhanar Sarsenova",
+    },
+    description: {
+      kz: "Директор по маркетингу",
+      ru: "Директор по маркетингу",
+      en: "Marketing Director",
+    },
+    whatsapp_number: "+77075214366",
+    mail_login: "mmukataev.02@gmail.com"
+  },
+
+  user_3: {
+    full_name: {
+      kz: "Аманжол Абдуллаев",
+      ru: "Аманжол Абдуллаев",
+      en: "Amanzhol Abdullayev",
+    },
+    description: {
+      kz: "Менеджер проектов",
+      ru: "Менеджер проектов",
+      en: "Project Manager",
+    },
+    whatsapp_number: "+77075214366",
+    mail_login: "mmukataev.02@gmail.com"
+  }
+};
